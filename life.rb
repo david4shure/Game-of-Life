dc5061db715ae4e571cf95ec502ed9cd885999eb
johnny4shure <dshure@westmont.edu>
2014-05-06 13:04:01 -0700

    Adding documentation

--- life.rb
+++ life.rb
@@ -2,15 +2,24 @@
 # David Shure
 # Conway's Game of Life
 
+# The Rules of John Conway's Game of Life:
+# ========================================
+# 1. Any live cell with fewer than two live neighbours dies, as if caused by under-population.
+# 2. Any live cell with two or three live neighbours lives on to the next generation.
+# 3. Any live cell with more than three live neighbours dies, as if by overcrowding.
+# 4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+
 $canvas = []
 $width = 0
 $height = 0
 
 
+# initializes board with life in random places according to spawn factor
 def init_canvas(spawn_factor)
   $canvas = Array.new($height) { Array.new($width) { Random.new.rand(1...100) < spawn_factor ? 1 : 0 } }
 end
 
+# prints canvas to stdout
 def print_canvas()
   for x in 0...$canvas.size()
     for y in 0...$height
@@ -20,6 +29,8 @@ def print_canvas()
   end
 end
 
+# According to the rules of the game of life
+# this function updates the canvas
 def update_canvas()
   for x in 0...$width
     for y in 0...$height
@@ -39,10 +50,19 @@ def update_canvas()
   end
 end
 
+# The (somewhat) tricky part
+# This function figures out how many neighbors
+# a cell at (x, y) has
 def count_neighbors(x, y)
   return count_left(x, y) + count_right(x, y) + count_up(x, y) + count_down(x, y) + count_diagonals(x, y)
 end
 
+# =================
+# Helper functions
+# =================
+
+# Counts the neighbor to the square
+# directly to the left of the current cell
 def count_left(x, y)
   if x - 1 < 0
     return 0
@@ -50,6 +70,8 @@ def count_left(x, y)
   return $canvas[x - 1][y] == 1 ? 1 : 0
 end
 
+# Counts the neighbor to the square
+# directly to the right of the current cell
 def count_right(x, y)
   if x + 1 >= $width
     return 0
@@ -57,6 +79,8 @@ def count_right(x, y)
   return $canvas[x + 1][y] == 1 ? 1 : 0
 end
 
+# Counts the neighbor to the square
+# directly up from the current cell
 def count_up(x, y)
   if y + 1 >= $height
     return 0
@@ -64,6 +88,8 @@ def count_up(x, y)
   return $canvas[x][y + 1] == 1 ? 1 : 0
 end
 
+# Counts the neighbors to the square
+# directly down from the current cell
 def count_down(x, y)
   if y - 1 < 0
     return 0
@@ -71,17 +97,26 @@ def count_down(x, y)
   return $canvas[x][y - 1] == 1 ? 1 : 0
 end
 
+
+# Counts the neighbors to the squares
+# that are diagonal to the square at (x, y)
 def count_diagonals(x, y)
   total_count = 0
-
+  
+  # Upper Left
   if x - 1 >= 0 and y + 1 < $height and $canvas[x - 1][y + 1] == 1 then total_count += 1 end
+  # Bottom Left
   if x - 1 >= 0 and y - 1 >= 0 and $canvas[x - 1][y - 1] == 1 then total_count += 1 end
+  # Upper Right
   if x + 1 < $width and y + 1 < $height and $canvas[x + 1][y + 1] == 1  then total_count += 1 end
+  # Bottom Right
   if x + 1 < $width and y - 1 >= 0 and $canvas[x + 1][y - 1] == 1 then total_count += 1 end
 
   return total_count
 end
 
+
+# Calling function
 def main()
   print "Enter board width (integer): "
   $width = gets.chomp().to_i
